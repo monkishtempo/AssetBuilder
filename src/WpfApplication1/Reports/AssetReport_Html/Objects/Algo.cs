649ec307e79133dcc3fdabeb43b131bdd46db440
mike.thompson <mike.thompson@healthhero.com>
2022-06-13 10:48:06 +0100

    Adding files again

---
+++ src/WpfApplication1/Reports/AssetReport_Html/Objects/Algo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AssetBuilder.Reports
+{
+    public class ARAlgo : BaseXmlObject
+    {
+        public override int ID => AlgoID;
+        public int AlgoID { get; set; }
+        public string Algo_Name { get; set; }
+        public string Word_Merge { get; set; }
+        public string WM2 { get; set; }
+        public string Algo_Name_Language { get; set; }
+        public string Word_Merge_Language { get; set; }
+        public string WM2_Language { get; set; }
+
+        public ARAlgo(XElement data) : base(data) { Type = "Algo"; }
+    }
+}
---
+++ src/WpfApplication1/Reports/AssetReport_Html/Objects/BaseXmlObject.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AssetBuilder.Reports
+{
+    public class BaseXmlObject
+    {
+        public virtual int ID { get; }
+        public string Type { get; set; }
+
+        public BaseXmlObject(XElement data)
+        {
+            var props = this.GetType().GetProperties();
+            foreach (var field in data.Elements())
+            {
+                var match = props.FirstOrDefault(p => p.Name == field.Name.ToString().Replace("_x0020_", ""));
+                if (match != null)
+                {
+                    if (match.PropertyType.IsEnum)
+                    {
+                        var value = Enum.ToObject(match.PropertyType, Convert.ChangeType(field.Value, typeof(int)));
+                        match.SetValue(this, value);
+                    }
+                    else
+                    {
+                        var value = Convert.ChangeType(field.Value, match.PropertyType);
+                        match.SetValue(this, value);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Prop> GetFields(string suffix = "_Language")
+        {
+            Type type = this.GetType();
+            var props = type.GetProperties().Where(f => f.Name.EndsWith(suffix))
+                .Select(f => new Prop { source = type.GetProperty(f.Name.Substring(0, f.Name.Length - suffix.Length)), target = f });
+            return props;
+        }
+
+        public void NLExpand(string suffix = "_Language")
+        {
+            var props = GetFields();
+            foreach (var prop in props)
+            {
+                var t = prop.source.GetValue(this);
+                t = AssetBuilder.Controls.NLTest.GetNLConditionString((string)t);
+                prop.source.SetValue(this, t);
+            }
+        }
+
+        public void Munge(BaseXmlObject obj, string suffix = "_Language")
+        {
+            var props = GetFields();
+            foreach (var prop in props)
+            {
+                if (prop.target != null)
+                {
+                    var t = prop.source.GetValue(this);
+                    var s = prop.source.GetValue(obj);
+                    if((string)s != (string)t)
+                        prop.target.SetValue(this, AssetBuilder.Controls.NLTest.GetNLConditionString((string)s));
+                    if (t != null)
+                    {
+                        t = AssetBuilder.Controls.NLTest.GetNLConditionString((string)t);
+                        prop.source.SetValue(this, t);
+                    }
+                }
+            }
+        }
+
+        public class Prop
+        {
+            public PropertyInfo source { get; set; }
+            public PropertyInfo target { get; set; }
+        }
+    }
+}
